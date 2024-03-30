e34ff415680099fd036b5322c118de9e19e1c1fb
shilathayayosefi <158490554+shilathayayosefi@users.noreply.github.com>
2024-03-30 03:09:59 +0300

    Update chat.py

--- chat.py
+++ chat.py
@@ -35,5 +35,5 @@ def chat():
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-if __name__ == '__main__':
+if __name__ == '__main__': 
     app.run(debug=True)
