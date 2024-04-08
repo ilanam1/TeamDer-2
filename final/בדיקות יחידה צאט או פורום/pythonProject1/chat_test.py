5341f3b3d3bcab4f39e101c69d17b99c6fde9cbe
shilathayayosefi <158490554+shilathayayosefi@users.noreply.github.com>
2024-04-09 02:48:45 +0300

    Update chat_test.py

--- final/בדיקות יחידה צאט או פורום/pythonProject1/chat_test.py
+++ final/בדיקות יחידה צאט או פורום/pythonProject1/chat_test.py
@@ -1,13 +1,11 @@
 import unittest
 from app import app
-
+import os
 class FlaskChatTestCase(unittest.TestCase):
-
     def setUp(self):
         # יצירת לקוח טסט עבור האפליקציה והגדרת סביבת טסט
         self.app = app.test_client()
         self.app.testing = True
-
 
     def test_index_page(self):
         # בודק אם הדף הראשי של האפליקציה נגיש ומכיל את הטקסט " Chat Group"
@@ -15,11 +13,19 @@ class FlaskChatTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Chat Group', response.data)
 
-    def test_send_message(self):
-        # בודק אם הודעה נשלחת לשרת ומקבלת תגובה תקינה עם פרטי ההודעה
-        response = self.app.post('/send', data={'username': 'test_user', 'message': 'Hello, world!'})
+    def test_send_message_with_image(self):
+        # בודק אם הודעה ותמונה נשלחות לשרת ומקבלות תגובה תקינה
+        # יצירת תמונת קובץ זמנית
+        with open('test_image.jpg', 'wb') as f:
+            f.write(b'fake_image_data')
+        # שליחת בקשת POST עם הודעה ותמונה
+        with open('test_image.jpg', 'rb') as f:
+            response = self.app.post('/send', data={'username': 'test_user', 'message': 'Hello, world!', 'file': f})
+        # בדיקת תגובה מהשרת
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'"message": "Hello, world!"', response.data)
+        self.assertEqual(response.data, b'Message sent successfully')
+        # מחיקת התמונה לאחר הבדיקה
+        os.remove('test_image.jpg')
 
     def test_upload_file(self):
         # בדיקה שהשרת מקבל קובץ מצורף מהמשתמש ושומר אותו בתיקיית ההעלאות
